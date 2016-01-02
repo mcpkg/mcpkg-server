53afd0aef0f4b8da07ba42be3b65eca8cfd73a29
Remy Goldschmidt <taktoa@gmail.com>
2016-01-02 13:01:42 -0600

    Fixed build for ipfs/Base58.kt

--- src/main/kotlin/ipfs/Base58.kt
+++ src/main/kotlin/ipfs/Base58.kt
@@ -19,70 +19,93 @@ package org.ipfs.api;
 import java.math.BigInteger;
 
 /**
- * A custom form of base58 is used to encode BitCoin addresses. Note that this is not the same base58 as used by
- * Flickr, which you may see reference to around the internet.<p>
+ * A custom form of base58 is used to encode BitCoin addresses.
  *
- * Satoshi says: why base-58 instead of standard base-64 encoding?<p>
+ * Note that this is not the same base58 as used by Flickr,
+ * which you may see reference to around the internet.
+ *
+ * <p>Satoshi says: why base-58 instead of standard base-64 encoding?<p>
  *
  * <ul>
  * <li>Don't want 0OIl characters that look the same in some fonts and
- *    could be used to create visually identical looking account numbers.</li>
- * <li>A string with non-alphanumeric characters is not as easily accepted as an account number.</li>
- * <li>E-mail usually won't line-break if there's no punctuation to break at.</li>
- * <li>Doubleclicking selects the whole number as one word if it's all alphanumeric.</li>
+ *     could be used to create visually identical looking account numbers.</li>
+ * <li>A string with non-alphanumeric characters is not as easily accepted
+ *     as an account number.</li>
+ * <li>E-mail usually won't line-break without punctuation to break at.</li>
+ * <li>Double-clicking and other forms of selection tend to select the whole
+ *     number as one word if it's all alphanumeric.</li>
  * </ul>
  */
-public class Base58 {
-    private static final String ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-    private static final BigInteger BASE = BigInteger.valueOf(58);
 
-    public static String encode(byte[] input) {
+public class Base58 {
+    private val ALPHABET
+      = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+    private val BASE = BigInteger.valueOf(58);
+
+    public fun encode(input: ByteArray): String {
         // TODO: This could be a lot more efficient.
-        BigInteger bi = new BigInteger(1, input);
-        StringBuffer s = new StringBuffer();
-        while (bi.compareTo(BASE) >= 0) {
-            BigInteger mod = bi.mod(BASE);
+        var bi = BigInteger(1, input);
+        val s = StringBuffer();
+        while(bi.compareTo(BASE) >= 0) {
+            val mod = bi.mod(BASE);
             s.insert(0, ALPHABET.charAt(mod.intValue()));
             bi = bi.subtract(mod).divide(BASE);
         }
         s.insert(0, ALPHABET.charAt(bi.intValue()));
         // Convert leading zeros too.
-        for (byte anInput : input) {
-            if (anInput == 0)
+        for(anInput in input) {
+            if((anInput as Int) == 0) {
                 s.insert(0, ALPHABET.charAt(0));
-            else
+            } else {
                 break;
+            }
         }
         return s.toString();
     }
 
-    public static byte[] decode(String input) {
-        byte[] bytes = decodeToBigInteger(input).toByteArray();
-        // We may have got one more byte than we wanted, if the high bit of the next-to-last byte was not zero. This
-        // is because BigIntegers are represented with twos-compliment notation, thus if the high bit of the last
-        // byte happens to be 1 another 8 zero bits will be added to ensure the number parses as positive. Detect
-        // that case here and chop it off.
-        boolean stripSignByte = bytes.length > 1 && bytes[0] == 0 && bytes[1] < 0;
+    public fun decode(input: String): ByteArray {
+        val bytes = decodeToBigInteger(input).toByteArray();
+
+        // We may have got one more byte than we wanted, if the high bit of the
+        // next-to-last byte was not zero. This is because BigIntegers are
+        // represented with twos-complement notation, thus if the high bit of
+        // the last byte happens to be 1 another 8 zero bits will be added to
+        // ensure the number parses as positive. Detect that case here and chop
+        // it off.
+        val stripSignByte =    bytes.size        >  1
+                            && (bytes[0] as Int) == 0
+                            && (bytes[1] as Int) <  0;
+
         // Count the leading zeros, if any.
-        int leadingZeros = 0;
-        for (int i = 0; input.charAt(i) == ALPHABET.charAt(0); i++) {
-            leadingZeros++;
+        var leading = 0;
+        var i = 0;
+        while(input.charAt(i) == ALPHABET.charAt(0)) {
+            leading++;
+            i++;
         }
-        // Now cut/pad correctly. Java 6 has a convenience for this, but Android can't use it.
-        byte[] tmp = new byte[bytes.length - (stripSignByte ? 1 : 0) + leadingZeros];
-        System.arraycopy(bytes, stripSignByte ? 1 : 0, tmp, leadingZeros, tmp.length - leadingZeros);
+
+        // Now cut/pad correctly.
+        // Java 6 has a convenience for this, but Android can't use it.
+        val strip = if(stripSignByte) 1 else 0;
+        val tmp = ByteArray(bytes.size - strip + leading);
+        System.arraycopy(bytes, strip, tmp, leading, tmp.size - leading);
+
         return tmp;
     }
 
-    public static BigInteger decodeToBigInteger(String input) {
-        BigInteger bi = BigInteger.valueOf(0);
+    public fun decodeToBigInteger(input: String): BigInteger {
+        var bi = BigInteger.valueOf(0);
         // Work backwards through the string.
-        for (int i = input.length() - 1; i >= 0; i--) {
-            int alphaIndex = ALPHABET.indexOf(input.charAt(i));
-            if (alphaIndex == -1) {
-                throw new IllegalStateException("Illegal character " + input.charAt(i) + " at " + i);
+        var i = input.length - 1;
+        while(i >= 0) {
+            val alphaIndex = ALPHABET.indexOf(input.charAt(i)) as Long;
+            if(alphaIndex == -1L) {
+                val errMsg = "Illegal character ${input.charAt(i)} at ${i}";
+                throw IllegalStateException(errMsg);
             }
-            bi = bi.add(BigInteger.valueOf(alphaIndex).multiply(BASE.pow(input.length() - 1 - i)));
+            val multiplier = BASE.pow(input.length - 1 - i);
+            bi = bi.add(BigInteger.valueOf(alphaIndex).multiply(multiplier));
+            i--;
         }
         return bi;
     }
