4d60a124dfc006b550c0c189dfe3285f3d2b0ef2
Remy Goldschmidt <taktoa@gmail.com>
2016-01-02 13:57:18 -0600

    Fixed build for ipfs/Multihash.kt

--- src/main/kotlin/ipfs/Multihash.kt
+++ src/main/kotlin/ipfs/Multihash.kt
@@ -25,97 +25,109 @@ package org.ipfs.api;
 import java.io.*;
 import java.util.*;
 
-public class Multihash {
-    public enum Type {
-        sha1(0x11, 20),
-        sha2_256(0x12, 32),
-        sha2_512(0x13, 64),
-        sha3(0x14, 64),
-        blake2b(0x40, 64),
-        blake2s(0x41, 32);
+public enum class MultihashType(index: Int, length: Int) {
+    sha1(0x11, 20),
+    sha2_256(0x12, 32),
+    sha2_512(0x13, 64),
+    sha3(0x14, 64),
+    blake2b(0x40, 64),
+    blake2s(0x41, 32);
 
-        public int index, length;
+    public val index = index;
+    public val length = length;
 
-        Type(int index, int length) {
-            this.index = index;
-            this.length = length;
+
+    companion object Static {
+        private val lookupM = TreeMap<Int, MultihashType>();
+
+        init {
+            for(t in MultihashType.values()) {
+                lookupM.put(t.index, t);
+            }
         }
 
-        private static Map<Integer, Type> lookup = new TreeMap<>();
-        static {
-            for (Type t: Type.values())
-                lookup.put(t.index, t);
+        public fun lookup(t: Int): MultihashType {
+            if(!lookupM.containsKey(t)) {
+                throw IllegalStateException("Unknown Multihash type: $t");
+            }
+            return (lookupM.get(t))!!;
         }
+    }
+}
 
-        public static Type lookup(int t) {
-            if (!lookup.containsKey(t))
-                throw new IllegalStateException("Unknown Multihash type: "+t);
-            return lookup.get(t);
+
+public class Multihash(type: MultihashType, hash: ByteArray) {
+    public val type = type;
+    public val hash = hash;
+
+    init {
+        val hl = hash.size;
+        val tl = type.length;
+        if(hl > 127) {
+            throw IllegalStateException("Unsupported hash size: $hl");
+        }
+        if(hl != tl) {
+            throw IllegalStateException("Incorrect hash length: $hl != $tl");
         }
     }
 
-    public final Type type;
-    public final byte[] hash;
-
-    public Multihash(Type type, byte[] hash) {
-        if (hash.length > 127)
-            throw new IllegalStateException("Unsupported hash size: "+hash.length);
-        if (hash.length != type.length)
-            throw new IllegalStateException("Incorrect hash length: " + hash.length + " != "+type.length);
-        this.type = type;
-        this.hash = hash;
+    companion object Static {
+        public fun fromBase58(base58: String): Multihash {
+            return Multihash(Base58().decode(base58));
+        }
     }
 
-    public Multihash(byte[] multihash) {
-        this(Type.lookup(multihash[0] & 0xff), Arrays.copyOfRange(multihash, 2, multihash.length));
-    }
+    constructor(multihash: ByteArray) :
+        this(MultihashType.lookup((multihash[0] as Int) and 0xff),
+             Arrays.copyOfRange(multihash, 2, multihash.size)) {}
 
-    public byte[] toBytes() {
-        byte[] res = new byte[hash.length+2];
-        res[0] = (byte)type.index;
-        res[1] = (byte)hash.length;
-        System.arraycopy(hash, 0, res, 2, hash.length);
+    public fun toBytes(): ByteArray {
+        val res = ByteArray(hash.size + 2);
+        res[0] = type.index as Byte;
+        res[1] = hash.size as Byte;
+        System.arraycopy(hash, 0, res, 2, hash.size);
         return res;
     }
 
-    @Override
-    public String toString() {
+    public override fun toString(): String {
         return toBase58();
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Multihash))
+    public fun equals(o: Any): Boolean {
+        if(o is Multihash) {
+            return (type == o.type && Arrays.equals(hash, o.hash));
+        } else {
             return false;
-        return type == ((Multihash) o).type && Arrays.equals(hash, ((Multihash) o).hash);
+        }
     }
 
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(hash) ^ type.hashCode();
+    public override fun hashCode(): Int {
+        return Arrays.hashCode(hash) xor type.hashCode();
     }
 
-    public String toHex() {
-        StringBuilder res = new StringBuilder();
-        for (byte b: toBytes())
-            res.append(String.format("%x", b&0xff));
+    public fun toHex(): String {
+        val res = StringBuilder();
+        for(b in toBytes()) {
+            res.append("%x".format((b as Int) and 0xff));
+        }
         return res.toString();
     }
 
-    public String toBase58() {
-        return Base58.encode(toBytes());
+    public fun toBase58(): String {
+        return Base58().encode(toBytes());
     }
 
-    public static Multihash fromHex(String hex) {
-        if (hex.length() % 2 != 0)
-            throw new IllegalStateException("Uneven number of hex digits!");
-        ByteArrayOutputStream bout = new ByteArrayOutputStream();
-        for (int i=0; i < hex.length()-1; i+= 2)
-            bout.write(Integer.valueOf(hex.substring(i, i+2), 16));
-        return new Multihash(bout.toByteArray());
+    public fun fromHex(hex: String): Multihash {
+        if(hex.length() % 2 != 0) {
+            throw IllegalStateException("Uneven number of hex digits!");
+        }
+        val bout = ByteArrayOutputStream();
+        var i = 0;
+        while(i < hex.length() - 1) {
+            bout.write(Integer.valueOf(hex.substring(i, i + 2), 16));
+            i += 2;
+        }
+        return Multihash(bout.toByteArray());
     }
 
-    public static Multihash fromBase58(String base58) {
-        return new Multihash(Base58.decode(base58));
-    }
 }
